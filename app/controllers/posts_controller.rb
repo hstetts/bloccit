3352b07fee56331d4221e937f1374811e6d69d93
hstetts@gmail.com <hstetts@gmail.com>
2018-01-28 14:48:48 -0500

    Bloccit CRUD Assignment: completed and updated notes in index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,13 @@ class PostsController < ApplicationController
     #of Post objects using all method provided by ActiveRecord
     #all returns a collection of Post objects
     @posts = Post.all
+
+    #iterate over posts and get index to update each 5th index with SPAM
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+       post.title = "SPAM"
+      end
+    end
   end
 
   def show
