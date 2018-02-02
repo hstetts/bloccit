ef4d537ba1a11f407d3c4d932fd8d1d796df32a1
hstetts@gmail.com <hstetts@gmail.com>
2018-02-02 12:55:52 -0500

    Bloccit Assignment More Crud: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :questions
+
   #Call the resources method and pass it a symbol that instructs Rails
   #to create post routes for creating, updating, viewing and deleting
   #instances of Post
--- lib/random_data.rb
+++ lib/random_data.rb
@@ -33,4 +33,8 @@ module RandomData
    letters.shuffle!
    letters[0,rand(3..8)].join
  end
+
+ def self.random_boolean
+    rand(0..1) == 1
+ end
 end
