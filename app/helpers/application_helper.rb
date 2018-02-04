226b75423bef436f4bc3287ca25220283bd28793
hstetts@gmail.com <hstetts@gmail.com>
2018-02-04 11:04:28 -0500

    Bloccit Checkpoint Validation: Completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+   #define a method named form_group_tag which takes two args, array of errors and block
+   #&turns block into a proc, which is a block that can be reused like a variable
+   def form_group_tag(errors, &block)
+     css_class = 'form-group'
+     css_class << ' has-error' if errors.any?
+
+     content_tag :div, capture(&block), class: css_class
+   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   #provides methods to allow us to retrieve comments belonging to a post
   belongs_to :topic
   has_many :comments, dependent: :destroy
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+
 end
